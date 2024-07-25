5fea31607d69e51820361d9dede1ae0b6a81852a
Abhinash-Padhi <152775625+Abhinash-Padhi@users.noreply.github.com>
2024-07-25 19:07:21 +0530

    Added Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Link, useLocation } from 'react-router-dom';
 import './header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUser, faInfoCircle, faSuitcase, faEnvelope, faSignOutAlt, faTimes, faOutdent } from '@fortawesome/free-solid-svg-icons';
@@ -6,6 +7,7 @@ import logo from './logo.png';
 
 const Header = () => {
     const [isNavActive, setIsNavActive] = useState(false);
+    const location = useLocation();
 
     const handleBarClick = () => {
         setIsNavActive(true);
@@ -21,12 +23,12 @@ const Header = () => {
                 <a href="#"><img src={logo} alt="Logo" className="logo" /></a>
                 <div>
                     <ul id="navbar" className={isNavActive ? 'active' : ''}>
-                        <li><a className="active" href="./Home"><FontAwesomeIcon icon={faHome} /> Home</a></li>
-                        <li><a href="./About"><FontAwesomeIcon icon={faInfoCircle} /> About Us</a></li>
-                        <li><a href="./Profile"><FontAwesomeIcon icon={faUser} /> Profile</a></li>
-                        <li><a href="./Tour"><FontAwesomeIcon icon={faSuitcase} /> Tour</a></li>
-                        <li><a href="./Contact"><FontAwesomeIcon icon={faEnvelope} /> Contact Us</a></li>
-                        <li><a href="./SignUp"><button><FontAwesomeIcon icon={faSignOutAlt} /> Sign Up</button></a></li>
+                        <li><Link className={location.pathname === '/Home' ? 'active' : ''} to="/Home"><FontAwesomeIcon icon={faHome} /> Home</Link></li>
+                        <li><Link className={location.pathname === '/About' ? 'active' : ''} to="/About"><FontAwesomeIcon icon={faInfoCircle} /> About Us</Link></li>
+                        <li><Link className={location.pathname === '/Profile' ? 'active' : ''} to="/Profile"><FontAwesomeIcon icon={faUser} /> Profile</Link></li>
+                        <li><Link className={location.pathname === '/Tour' ? 'active' : ''} to="/Tour"><FontAwesomeIcon icon={faSuitcase} /> Tour</Link></li>
+                        <li><Link className={location.pathname === '/Contact' ? 'active' : ''} to="/Contact"><FontAwesomeIcon icon={faEnvelope} /> Contact Us</Link></li>
+                        <li><Link className={location.pathname === '/SignUp' ? 'active' : ''} to="/SignUp"><button><FontAwesomeIcon icon={faSignOutAlt} /> Sign Up</button></Link></li>
                         <a href="#" id="close" onClick={handleCloseClick}><FontAwesomeIcon icon={faTimes} /></a>
                     </ul>
                 </div>
@@ -37,6 +39,5 @@ const Header = () => {
         </div>
     );
 };
-
 export default Header;
 
