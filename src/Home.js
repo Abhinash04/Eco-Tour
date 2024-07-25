40acd7c67ed162ab213dc439b166a408d8aed95f
Abhinash04 <abhinash.pritiraj@gmail.com>
2024-07-25 21:35:43 +0530

    updated repo

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,9 @@
-import React from 'react';
-import './home.css';
+import React from 'react'
 
-function Home() {
+const Home = () => {
   return (
-    <div className="Home">
-      <main>
-        <section id="hero">
-          <h1>We travel not to escape life, but for life not to escape us and our planet.</h1>
-          <button>Know more</button>
-        </section>
-      </main>
-      <footer>
-        <p>&copy; 2024 ECOTOUR</p>
-      </footer>
-    </div>
-  );
+    <div>Home</div>
+  )
 }
 
-export default Home;
+export default Home
