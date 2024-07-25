3fd755d29b1645d31336f83351ea45f0e638a75c
Abhinash-Padhi <152775625+Abhinash-Padhi@users.noreply.github.com>
2024-07-25 19:04:03 +0530

    Placed Header

--- src/About.js
+++ src/About.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './about.css';
+import Header from './components/Header'
 
 const About = () => {
     return (
         <div className='ab-body'>
-            <header></header>
+            <Header/>
 
             <div className="abclass">
               
