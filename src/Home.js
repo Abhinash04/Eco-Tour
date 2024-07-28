d77ccc6e83fcb1a55a9525468bd817b72ea5f819
Abhinash-Padhi <152775625+Abhinash-Padhi@users.noreply.github.com>
2024-07-28 17:07:49 +0530

    Placed Footer

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import "./Home.css"
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const Home = () => {
     return (
-        <div className="home">
-            <div className="content">
-                <h5>Welcome To Eco-Tour</h5>
-                <h1>Visit <span className="changecontent"></span></h1>
-                <p>Explore the world sustainably with EcoTour - your guide to eco-friendly travel adventures</p>
-                <a href="#book" className="btn btn-primary">Book Place</a>
+        <div>
+            <div className="home">
+                <Header/>
+                <div className="content">
+                    <h5>Welcome To Eco-Tour</h5>
+                    <h1>Visit <span className="changecontent"></span></h1>
+                    <p>Explore the world sustainably with EcoTour - your guide to eco-friendly travel adventures</p>
+                    <a href="#book" className="btn btn-primary">Book Place</a>
+                </div>
             </div>
+        <Footer/>
         </div>
     );
 }
