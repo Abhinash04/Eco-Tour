76b91e5cb4bd2a512c3fbc1191a4a5cd1ffa2f34
Abhinash-Padhi <152775625+Abhinash-Padhi@users.noreply.github.com>
2024-08-03 17:02:43 +0530

    Routed all activities and accomodations

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ import Tips from "./Tips";
 import Review from "./Review";
 import Tour from "./Tour";
 import CEC from "./CarbonEmissionCalculator";
+import Act1 from "./KanyaKumari/Activities.js";
+import Acc1 from "./KanyaKumari/Accommodations.js";
+import Act2 from "./Kerala/Activities.js";
+import Acc2 from "./Kerala/Accommodations.js";
+import Act3 from "./Manali/Activities.js";
+import Acc3 from "./Manali/Accommodations.js";
+import Act4 from "./Nainital/Activities.js";
+import Acc4 from "./Nainital/Accommodations.js";
+import Act5 from "./Rishikesh/Activities.js";
+import Acc5 from "./Rishikesh/Accommodations.js";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
 import Toggle from "./components/Toggle";
@@ -23,6 +33,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/tour" element={<Tour />} />
@@ -31,6 +42,16 @@ const App = () => {
         <Route path="/tour" element={<Tour />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/cec" element={<CEC />} />
+        <Route path="/act1" element={<Act1 />} />
+        <Route path="/acc1" element={<Acc1 />} />
+        <Route path="/act2" element={<Act2 />} />
+        <Route path="/acc2" element={<Acc2 />} />
+        <Route path="/act3" element={<Act3 />} />
+        <Route path="/acc3" element={<Acc3 />} />
+        <Route path="/act4" element={<Act4 />} />
+        <Route path="/acc4" element={<Acc4 />} />
+        <Route path="/act5" element={<Act5 />} />
+        <Route path="/acc5" element={<Acc5 />} />
         <Route
           path="/signup"
           element={
