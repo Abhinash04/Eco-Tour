16081879bc2781fc430b1c1ad629441c1302f6e8
Subhra-Sarojini-Sahoo <152807439+Subhra-Sarojini-Sahoo@users.noreply.github.com>
2024-08-04 23:09:29 +0530

    Update Activities.js

--- src/Places/Kerala/Activities.js
+++ src/Places/Kerala/Activities.js
@@ -48,7 +48,7 @@ const activities = [
 
 const Activities = () => {
   return (
-    <div className=""page>
+    <div className="page">
     <div className="activities-container">
  
       <h2>Activities in Kerala</h2>
