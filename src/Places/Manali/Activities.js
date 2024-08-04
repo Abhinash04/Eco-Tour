94312e793415f92382e235386e7e9da5a340003a
Subhra-Sarojini-Sahoo <152807439+Subhra-Sarojini-Sahoo@users.noreply.github.com>
2024-08-04 19:51:49 +0530

    Updated Activities.js

--- src/Places/Manali/Activities.js
+++ src/Places/Manali/Activities.js
@@ -48,7 +48,7 @@ const activities = [
 
 const Activities = () => {
   return (
-    
+    <div className='page'>
     <div className="activities-container">
       <h1></h1>
       <h2>Activities in Manali</h2>
@@ -75,7 +75,7 @@ const Activities = () => {
         ))}
       </div>
     </div>
+    </div>
   );
 };
-
 export default Activities;
